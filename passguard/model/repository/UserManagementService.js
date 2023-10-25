eb04fcfa226055e1c169f23300c6a5418e6a790e
essa-ea2004969 <ea2004969@qu.edu.qa>
2023-10-25 21:46:01 +0300

    Added Required Repo Methods For (Trash & Credentials))

--- passguard/model/repository/UserManagementService.js
+++ passguard/model/repository/UserManagementService.js
@@ -7,4 +7,22 @@ export default class UserManagementService {
     //Add Methods Here for Creating, Updating, and Deleting
     //Use this example signature --> async createUser() {}
 
+    //add a new credential to the database
+    async createCredential(credential) {
+        return await prisma.CREDENTIAL.create({
+            data: credential
+        })
+    }
+
+    //delete a credential by credential id
+    async deleteCredentialById(credentialId) {
+        return await prisma.CREDENTIAL.delete({
+            where: {
+                CREDENTIAL_ID: credentialId
+            }
+        })
+    }
+
+
+
 }
--- passguard/model/repository/UserQueryService.js
+++ passguard/model/repository/UserQueryService.js
@@ -7,4 +7,96 @@ export default class UserManagementService {
     //Add Methods Here for Reading 
     //Use this example signature --> async getUsers() {}
 
+    
+    //get user credentials by user id
+    async getCredentialsByUserId(userId) {
+        return await prisma.CREDENTIAL.findMany({
+            where: {
+                USER_ID: userId
+            }
+        })
+    }
+
+    //get a credential by credential id
+    async getCredentialById(credentialId) {
+        return await prisma.CREDENTIAL.findUnique({
+            where: {
+                CREDENTIAL_ID: credentialId
+            }
+        })
+    }
+
+    //get credentials by service
+    async getCredentialsByService(service) {
+        return await prisma.CREDENTIAL.findMany({
+            where: {
+                CREDENTIAL_SERVICE: service
+            }
+        })
+    }
+
+    //get credential by title
+    async getCredentialsByTitle(title) {
+        return await prisma.CREDENTIAL.findMany({
+            where: {
+                CREDENTIAL_TITLE: title
+            }
+        })
+    }
+
+    //get credential by URL
+    async getCredentialsByUrl(url) {
+        return await prisma.CREDENTIAL.findMany({
+            where: {
+                CREDENTIAL_URL: url
+            }
+        })
+    }
+
+    //get trash by trash id
+    async getTrashById(trashId) {
+        return await prisma.TRASH.findUnique({
+            where: {
+                TRASH_ID: trashId
+            }
+        })
+    }
+
+   //get credentials that are temporarily deleted from the database
+    async getTrash() {
+     return await prisma.TRASH.findMany()
+    }
+
+    //get credentials by date deleted from trash database
+    async getTrashByDate(date) {
+        return await prisma.TRASH.findMany({
+            where: {
+                TRASH_DATETEMPDELETED: date
+            }
+        })
+    }
+
+    //get credentials that are temporarily deleted after a specific date
+    async getTrashAfterDate(date) {
+        return await prisma.TRASH.findMany({
+            where: {
+                TRASH_DATETEMPDELETED: {
+                    gt: date
+                }
+            }
+        })
+    }
+
+    //get credentials that are temporarily deleted before a specific date
+    async getTrashBeforeDate(date) {
+        return await prisma.TRASH.findMany({
+            where: {
+                TRASH_DATETEMPDELETED: {
+                    lt: date
+                }
+            }
+        })
+    }
+
+
 }
