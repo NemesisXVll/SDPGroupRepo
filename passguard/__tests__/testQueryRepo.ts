5a182105dd20f1cfee51c9d32126f166e0f3c080
Nemesis <ya1901748@qu.edu.qa>
2024-02-11 20:50:42 +0300

    Home Change

--- passguard/__tests__/testQueryRepo.ts
+++ passguard/__tests__/testQueryRepo.ts
@@ -4,32 +4,33 @@ import { prismaMock } from "../singleton";
 const userQueryService = new UserQueryService();
 
 test("Should Properly Find a User By Id", async () => {
-  const userIdToFind = 3;
+	const userIdToFind = 3;
 
-  const existingUser = {
-    userId: userIdToFind,
-    data: JSON.stringify({
-      masterPassword: "nonofurbusiness369",
-      fName: "Essaa",
-      lName: "Ahmed",
-      email: "ea2004969@qu.edu.qa",
-    }),
-    salt: "et52ed",
-    dateCreated: new Date(),
-    dateUpdated: new Date(),
-    picture: "C:/Users/Essa/Mypicture.png",
-  };
+	const existingUser = {
+		userId: userIdToFind,
+		email: "ea2004969@qu.edu.qa",
+		data: JSON.stringify({
+			masterPassword: "nonofurbusiness369",
+			fName: "Essaa",
+			lName: "Ahmed",
+		}),
+		salt: "et52ed",
+		dateCreated: new Date(),
+		dateUpdated: new Date(),
+		picture: "C:/Users/Essa/Mypicture.png",
+	};
 
-  prismaMock.user.findUnique.mockResolvedValue(existingUser);
+	prismaMock.user.findUnique.mockResolvedValue(existingUser);
 
-  const foundUser = await userQueryService.findUserById(userIdToFind);
+	const foundUser = await userQueryService.findUserById(userIdToFind);
 
-  expect(foundUser).toMatchObject({
-    userId: userIdToFind,
-    data: existingUser.data,
-    salt: existingUser.salt,
-    dateCreated: expect.any(Date),
-    dateUpdated: expect.any(Date),
-    picture: existingUser.picture,
-  });
+	expect(foundUser).toMatchObject({
+		userId: userIdToFind,
+		email: existingUser.email,
+		data: existingUser.data,
+		salt: existingUser.salt,
+		dateCreated: expect.any(Date),
+		dateUpdated: expect.any(Date),
+		picture: existingUser.picture,
+	});
 });
--- passguard/src/components/Home.tsx
+++ passguard/src/components/Home.tsx
@@ -4,87 +4,110 @@ import Form from "./Form/Form.tsx";
 import Navbar from "./Navbar.tsx";
 import Stats from "./Stats.tsx";
 import Grid from "./CredentialSection/Grid.tsx";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { CredentialData } from "./CredentialSection/Grid.tsx";
+import CredentialService from "../utils/credentialService.ts";
+
+const userQueryService = new CredentialService();
 
 function Home() {
-  const [showForm, setShowForm] = useState(false);
-  const [editInput, setEditInput] = useState(false);
-  const [credential, setCredential] = useState<any>([]);
-  const [formSubmitted, setFormSubmitted] = useState(false);
+	const [showForm, setShowForm] = useState(false);
+	const [editInput, setEditInput] = useState(false);
+	const [credential, setCredential] = useState<any>([]);
+	const [formSubmitted, setFormSubmitted] = useState(false);
+	const [credentials, setCredentials] = useState<any>([{}]);
+	const [dataRequested, setDataRequested] = useState(false);
 
-  const location = useLocation();
-  const user = location.state.user;
+	const location = useLocation();
+	const user = location.state.user;
 
-  const handleAddClick = () => {
-    setCredential({});
-    setShowForm(false);
-    setEditInput(true);
-    // Introduce a delay before setting ShowForm to true
-    setTimeout(() => {
-      setShowForm(true);
-    }, 0); // Adjust the delay time (in milliseconds) according to your needs
-  };
-  // Callback function to be passed to Grid
-  const handleCardClickInApp = (
-    credentialData: CredentialData,
-    updateClicked: boolean
-  ) => {
-    setShowForm(false);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const credentials = await userQueryService.findCredentialsByUserId(
+					user.userId
+				);
+				setCredentials(credentials);
+				// console.log("hello", credentials);
+			} catch (error) {
+				console.error("error fetching credentials in home", error);
+			}
+		};
+		fetchData();
+	}, [dataRequested]);
+	const handleAddClick = () => {
+		setCredential({});
+		setShowForm(false);
+		setEditInput(true);
+		// Introduce a delay before setting ShowForm to true
+		setTimeout(() => {
+			setShowForm(true);
+		}, 0); // Adjust the delay time (in milliseconds) according to your needs
+	};
+	// Callback function to be passed to Grid
+	const handleCardClickInApp = (
+		credentialData: CredentialData,
+		updateClicked: boolean
+	) => {
+		setShowForm(false);
+		syncData();
 
-    // Introduce a delay before setting ShowForm to true
-    setTimeout(() => {
-      setShowForm(true);
-    }, 0); // Adjust the delay time (in milliseconds) according to your needs
-    setCredential(credentialData);
-    setEditInput(updateClicked);
-  };
+		// Introduce a delay before setting ShowForm to true
+		setTimeout(() => {
+			setShowForm(true);
+		}, 0); // Adjust the delay time (in milliseconds) according to your needs
+		setCredential(credentialData);
+		setEditInput(updateClicked);
+	};
 
-  const handleFormBTN = (showForm: boolean) => {
-    setShowForm(showForm);
-  };
+	const handleFormBTN = (showForm: boolean) => {
+		setShowForm(showForm);
+	};
 
-  function handleFormSubmitted(): void {
-    setFormSubmitted((formSubmitted) => !formSubmitted);
-  }
+	function handleFormSubmitted(): void {
+		setFormSubmitted((formSubmitted) => !formSubmitted);
+	}
+	function syncData() {
+		setDataRequested((dataRequested) => !dataRequested);
+	}
 
-  return (
-    <>
-      <div className="app-container h-screen">
-        <div className="navbar">
-          <Navbar isactive={false}></Navbar>
-        </div>
+	return (
+		<>
+			<div className="app-container h-screen">
+				<div className="navbar">
+					<Navbar isactive={false}></Navbar>
+				</div>
 
-        <div className="stats mb-5">
-          <Stats></Stats>
-        </div>
+				<div className="stats mb-5">
+					<Stats></Stats>
+				</div>
 
-        <div className="form">
-          {showForm ? (
-            <Form
-              userId={user.userId}
-              formSubmitted={handleFormSubmitted}
-              credentialObj={credential}
-              editable={editInput}
-              onBTNClick={handleFormBTN}
-            ></Form>
-          ) : (
-            ""
-          )}
-        </div>
+				<div className="form">
+					{showForm ? (
+						<Form
+							userId={user.userId}
+							formSubmitted={handleFormSubmitted}
+							credentialObj={credential}
+							editable={editInput}
+							onBTNClick={handleFormBTN}
+						></Form>
+					) : (
+						""
+					)}
+				</div>
 
-        <div className="credentials overflow-auto ml-4 mt-3">
-          <Grid
-            userId={user.userId}
-            onCardClick={handleCardClickInApp}
-            onAddClick={handleAddClick}
-            onFormSubmit={formSubmitted}
-          ></Grid>
-        </div>
-      </div>
-    </>
-  );
+				<div className="credentials overflow-auto ml-4 mt-3">
+					<Grid
+						userId={user.userId}
+						onCardClick={handleCardClickInApp}
+						onAddClick={handleAddClick}
+						onFormSubmit={formSubmitted}
+					></Grid>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default Home;
