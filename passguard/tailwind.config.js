37cbb580266f2284825b0b66dae6ef2b16ab5413
Nemesis <ya1901748@qu.edu.qa>
2024-03-31 18:25:35 +0300

    -added darkmode config colors

--- passguard/tailwind.config.js
+++ passguard/tailwind.config.js
@@ -13,14 +13,35 @@ module.exports = {
 			colors: {
 				yellow: {
 					400: "#facc15",
-					
 				},
 				blue: {
-					999: "rgb(55 65 81 / 1)"
+					999: "rgb(55 65 81 / 1)",
 				},
 				darkwhite: {
-					999: "#F5F5F5"
-				}
+					999: "#F5F5F5",
+				},
+				darkbg: { //background color for dark mode
+					999:"#010409"
+				},
+				darktext: {
+					// main text color for dark mode
+					999: "#e6edf3",
+				},
+				darksubtext: {
+					// sub text color for dark mode
+					999: "##848d97",
+				},
+				darkcards: {
+					// card background color for dark mode
+					999: "#161b22",
+				},
+				darkborder: {
+					// border color for dark mode
+					999: "#30363d",
+				},
+				darkinset: { //navbar and input fields
+					999: "#0d1117",
+				},
 			},
 		},
 	},
